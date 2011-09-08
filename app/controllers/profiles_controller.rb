58a8b9d23d0b27f80100bb83f57c1216afa27d54
Cyber <matthewlikesrobots@gmail.com>
2011-09-07 21:28:27 -0500

    Develop Branch ready for merge with master

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,7 @@ class ProfilesController < ApplicationController
 
   def show
    @user = User.find(params[:id])
+   @projects = Project.find_all_by_requester(@user.id, :order => "created_at DESC")
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+ def removehttp(text)
+  (text).gsub("http://", "").gsub("https://", "")
+ end
+ def is_user?
+  current_user.id == @requester.id
+ end 
 end
